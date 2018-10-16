194a79e87d1147b1567cfcc5075529fbed4d131f
Michael <mchen0358@gmail.com>
2018-10-16 16:53:59 +0700

    test lagi:

--- telegram.js
+++ telegram.js
@@ -8,7 +8,7 @@ var bot = new TelegramBot(token, {polling: true});
 bot.onText(/\/echo (.+)/, function (msg, match) {
   var fromId = msg.from.id;
   var resp = match[1];
-  bot.sendMessage(fromId, resp);
+  bot.sendMessage(fromId, resp + "lol");
 });
 
 // Any kind of message
